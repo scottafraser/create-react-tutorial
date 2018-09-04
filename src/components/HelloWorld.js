a0a3e471c29bb68dd12cf276ba1c3247b0ba62d8
Scott Fraser <scottafraser@yahoo.com>
2018-09-03 22:39:05 -0700

    finished part 3

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -4,20 +4,25 @@ import "./HelloWorld.css";
 class HelloWorld extends Component {
   constructor(props) {
     super(props);
-    this.state = { greeting: "Hello" };
+    this.state = { greeting: "Hello " };
     this.frenchify = this.frenchify.bind(this);
+    this.removeGreeting = this.removeGreeting.bind(this);
   }
   render() {
     return (
       <div className="HelloWorld">
-        {this.state.greeting}
-        {this.props.name}!<br />
+        {this.state.greeting} {this.props.name}!<br />
         <button onClick={this.frenchify}>Frenchify!</button>
+        <br />
+        <button onClick={this.removeGreeting}>Remove Me!</button>
       </div>
     );
   }
   frenchify() {
-    this.setState({ greeting: "Bonjour" });
+    this.setState({ greeting: "Bonjour " });
+  }
+  removeGreeting() {
+    this.props.removeGreeting(this.props.name);
   }
 }
 export default HelloWorld;
--- src/components/HelloWorldList.js
+++ src/components/HelloWorldList.js
@@ -1,16 +1,41 @@
 import React, { Component } from "react";
 import "./HelloWorldList.css";
+import AddGreeter from "./AddGreeter";
 
 import HelloWorld from "./HelloWorld";
 
 class HelloWorldList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { greetings: ["Jim", "Sally", "Bender"] };
+    this.addGreeting = this.addGreeting.bind(this);
+    this.removeGreeting = this.removeGreeting.bind(this);
+  }
+
   render() {
     return (
       <div className="HelloWorldList">
-        <HelloWorld name="Jim" />
-        <HelloWorld name="Sally" />
+        <AddGreeter addGreeting={this.addGreeting} />
+        {this.renderGreetings()}
       </div>
     );
+  }
+
+  renderGreetings() {
+    return this.state.greetings.map(name => (
+      <HelloWorld key={name} name={name} removeGreeting={this.removeGreeting} />
+    ));
+  }
+
+  addGreeting(newName) {
+    this.setState({ greetings: [...this.state.greetings, newName] });
+  }
+
+  removeGreeting(removeName) {
+    const filteredGreetings = this.state.greetings.filter(name => {
+      return name !== removeName;
+    });
+    this.setState({ greetings: filteredGreetings });
   }
 }
 export default HelloWorldList;
