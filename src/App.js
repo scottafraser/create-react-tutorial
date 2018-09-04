8b5bd611ce7eb180ae1f20b87c80efc085dbe73e
Scott Fraser <scottafraser@yahoo.com>
2018-09-03 21:49:18 -0700

    beginning part 3

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import HelloWorld from "./HelloWorld";
+
+const App = () => {
+  return (
+    <div className="App">
+      <HelloWorld name="Jim" />
+      <HelloWorld name="Bob" />
+    </div>
+  );
+};
+export default App;
